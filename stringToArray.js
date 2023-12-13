9a3c6e0217f3ab3ff51d734f7fa0de69073dc414
Andrei Siarheyenka <andrew.srgn@gmail.com>
2023-12-13 12:15:53 -0800

    TIK-004/ Fix func arrayToString

--- stringToArray.js
+++ stringToArray.js
@@ -1,10 +1,9 @@
 const stringToArray = (string) => {
     if (typeof string !== 'string') return 'Not a string';
     if (string.length === 0) return 'Empty string';
+    if (!string.split('').every(char => {return char >= '0' && char <= '9'})) return 'Not a string of numbers';
 
-    const array = string.split('');
-
-    return array.every(char => {return char >= '0' && char <= '9'}) ? array : 'Not a string of numbers';
+    return  string.split('').map(char => Number(char));
 }
 
 module.exports = stringToArray;
